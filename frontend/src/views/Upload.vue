55ce85778085a6e76c256581fde4da1c92424900
alkl58 <alkl58@protonmail.com>
2025-02-21 19:39:49 +0100

    Refactor Error message in own component

--- frontend/src/views/Upload.vue
+++ frontend/src/views/Upload.vue
@@ -13,18 +13,7 @@
       </div>
 
       <!-- Error -->
-      <div v-if="error" class="flex rounded bg-red-400 text-red-900 min-h-[48px] mb-4">
-        <div class="self-center p-2">
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none"
-            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-            class="icon icon-tabler icons-tabler-outline icon-tabler-x">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M18 6l-12 12" />
-            <path d="M6 6l12 12" />
-          </svg>
-        </div>
-        <p class="self-center">Encountered error: {{ error }}</p>
-      </div>
+      <ErrorMessage :message="error" />
 
       <!-- Success -->
       <div v-if="bucketUrl" class="flex rounded bg-lime-500 text-lime-900 min-h-[48px] mb-4">
@@ -175,8 +164,12 @@
 
 <script>
 import * as tus from 'tus-js-client';
+import ErrorMessage from './components/ErrorMessage.vue';
 
 export default {
+  components: {
+    ErrorMessage
+  },
   data() {
     return {
       uploading: false,
