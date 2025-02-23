b468ffa3fe1d8850d2160a2c07b6f4a94037fd5c
alkl58 <alkl58@protonmail.com>
2025-02-23 12:37:28 +0100

    Fix imports

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
 
 // Middleware for token verification
 const verifyToken = (req, res, next) => {
@@ -11,14 +13,14 @@ const verifyToken = (req, res, next) => {
     jwt.verify(token, process.env.PRIVATE_KEY);
     next(); // Proceed to the next middleware/route handler
   } catch (err) {
-    console.error(`Invalid token ${token}!`);
+    console.error(`Invalid token ${token}!`, err);
     return res.status(401).send('Invalid token!');
   }
 };
 
 const verifyFileSize = (req, res, next) => {
   const size = req.headers['upload-length'];
-  if (MAX_FILE_SIZE !== -1 && MAX_FILE_SIZE < Number(size)) {
+  if (config.MAX_FILE_SIZE !== -1 && config.MAX_FILE_SIZE < Number(size)) {
     return res.status(400).send('File too big!');
   }
   next();
